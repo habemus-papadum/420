cea194418555d4df3251377e9376c3ef06cdd9d1
Nehal Patel <nehal@alum.mit.edu>
2022-04-15 09:28:45 -0400

    add base64, todos, internalize data_url

--- src/jl420.jl
+++ src/jl420.jl
@@ -1,5 +1,21 @@
 module jl420
 
-greet() = print("Hello World!")
+using HypertextLiteral
+using Base64
+
+function data_url(f)
+    io = IOBuffer();
+    write(io,"data:image/png;base64,")
+    iob64 = Base64EncodePipe(io)
+    show(iob64, "image/png", f)
+    String(take!(io))
+end
+
+
+for n in names(@__MODULE__; all=true)
+    if Base.isidentifier(n) && n ∉ (Symbol(@__MODULE__), :eval, :include)
+        @eval export $n
+    end
+end
 
 end # module
